9a9883c816b4767736fe1f99360ac6aee3191094
nathanaelraj <nathanae001@e.ntu.edu.sg>
2018-10-23 22:02:18 +0800

    Performed login verfiifcation, included verifyrole decorator

--- scse_cz3003_2018_s1_cms_app/views/login.py
+++ scse_cz3003_2018_s1_cms_app/views/login.py
@@ -1,8 +1,29 @@
 from django.views.decorators.csrf import csrf_exempt
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 
+
+def verifyRole(view):
+    def executeView(*args, **kwargs):
+        result = view(*args, **kwargs)
+        request = args[0]
+        if 'role' in kwargs.keys():
+            role = kwargs['role']
+        else:
+            role = 'cms'
+        cookie = request.COOKIES.get('auth')
+        dictToSend = {
+            'cookie': cookie
+        }
+        authenticatedRole = requests.post('http://localhost:5002/checkCookie', json=dictToSend)
+        print('role:', authenticatedRole.text)
+        if role != authenticatedRole.text:
+            print('incorrect role')
+            return HttpResponse('You do not have permission to access this webpage')
+        else:
+            return result
+    return executeView
 
 ########################################################################################################################
 # Views
@@ -13,9 +34,9 @@ def login(request):
     return render(request, 'login.html')
 
 
+@verifyRole
 def home(request):
     return render(request, 'home.html', {'page_name': "Homepage"})
-
 
 
 ########################################################################################################################
@@ -25,29 +46,22 @@ def home(request):
 @csrf_exempt
 def authorization(request):
     if request.method == 'POST':
-        try:
+        # try:
             # Log the user in
-            dictToSend = {
-                'username': request.POST.get('username'),
-                'password': request.POST.get('password')
-            }
-            print(dictToSend)
-            session_cookie = requests.post('http://localhost:5002/login', json=dictToSend)
-
-            print('session_cookie', session_cookie.text[:10])
-            if not (session_cookie.text == 'error'):
-                print('creating session')
-                response = HttpResponse("successful")
-
-                response.set_cookie('auth', session_cookie.text)
-
-                print("session created")
-
-                return home(request)
-        except Exception:
-            return HttpResponse("fail")
-    else:
-        return HttpResponse("fail")
+        dictToSend = {
+            'username': request.POST.get('username'),
+            'password': request.POST.get('password')
+        }
+        session_cookie = requests.post('http://localhost:5002/login', json=dictToSend)
+        print('session_cookie', session_cookie.text[:10])
+        if not (session_cookie.text == 'error'):
+            # response = HttpResponse("successful")
+            response = redirect('home')
+            response.set_cookie('auth', session_cookie.text)
+            return response
+        # except Exception:
+        #     return HttpResponse("fail")
+    return HttpResponse("fail")
 
 
 
