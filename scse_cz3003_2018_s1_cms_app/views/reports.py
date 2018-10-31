96997e7fd6ddc261890ae5d6c8761d018db24fc8
jordan <jordanlam009@hotmail.com>
2018-10-31 14:57:58 +0800

    mock data in incidents

--- scse_cz3003_2018_s1_cms_app/views/reports.py
+++ scse_cz3003_2018_s1_cms_app/views/reports.py
@@ -66,7 +66,7 @@ def get_allincidentreport(request):
         ir['latitude'] = str(ir['latitude'])
         ir['crisis_level_id'] = CrisisLevel.objects.get(id=ir['crisis_level_id']).name
         ir['source_id'] = Source.objects.get(id=ir['source_id']).name
-
+    print("getting all incidents",list(all_incident_reports))
     return HttpResponse(json.dumps(list(all_incident_reports)), content_type='application/json')
 
 def validate_incidentreport(request):
@@ -204,4 +204,3 @@ def add_incidentreport(request):
 
             ir.save()
             return HttpResponse('successful')
-
